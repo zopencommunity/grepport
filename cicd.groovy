21a46b536f20e823bfc31459a32bf2ddf790dbb3
Igor Todorovski <itodorov@ca.ibm.com>
2024-09-20 17:20:49 -0400

    Change organization to zopencommunity

--- cicd.groovy
+++ cicd.groovy
@@ -7,9 +7,9 @@ node('linux')
                         branches: [[name: '*/main']],
                         doGenerateSubmoduleConfigurations: false,
                         extensions: [],
-                        userRemoteConfigs: [[url: "https://github.com/ZOSOpenTools/grepport.git"]]])
+                        userRemoteConfigs: [[url: "https://github.com/zopencommunity/grepport.git"]]])
         }
   stage('Build') {
-    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/grepport.git'), string(name: 'PORT_DESCRIPTION', value: 'Grep searches one or more input files for lines containing a match to a specified pattern. By default, Grep outputs the matching lines.' )]
+    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/zopencommunity/grepport.git'), string(name: 'PORT_DESCRIPTION', value: 'Grep searches one or more input files for lines containing a match to a specified pattern. By default, Grep outputs the matching lines.' )]
   }
 }
